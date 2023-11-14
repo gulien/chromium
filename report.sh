040d245e186af1691e3698084f6792209e70adc4
Julien Neuhart <neuhart.julien@gmail.com>
2023-11-14 13:14:05 +0100

    feat: add bash -x /usr/bin/chromium --version output

--- report.sh
+++ report.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 CHROMIUM_VERSION=$(chromium --version 2>&1)
+BASH_CHROMIUM_VERSION=$(bash -x /usr/bin/chromium --version 2>&1)
 KERNEL=$(uname -a)
 CPU_INFO=$(cat /proc/cpuinfo)
 
 read -r -d '' OUTPUT << EOM
 chromium --version:
 $CHROMIUM_VERSION
+
+bash -x /usr/bin/chromium --version:
+$BASH_CHROMIUM_VERSION
 
 uname -a:
 $KERNEL
